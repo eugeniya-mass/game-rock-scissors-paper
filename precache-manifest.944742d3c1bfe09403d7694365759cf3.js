1a48bde2a16c83a7f91de7bf525cdcc539e8755e
jijek-com <mas.eug@yandex.ru>
2026-02-13 04:41:37 +0500

    Updates

---
+++ precache-manifest.944742d3c1bfe09403d7694365759cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9ce123fd8cf874bc8a6014546325b6",
+    "url": "/game-rock-scissors-paper/index.html"
+  },
+  {
+    "revision": "b5dbef5ab69e88d31432",
+    "url": "/game-rock-scissors-paper/static/css/main.1a0fddec.chunk.css"
+  },
+  {
+    "revision": "1618da1407d8bfde7de8",
+    "url": "/game-rock-scissors-paper/static/js/2.a63da593.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/game-rock-scissors-paper/static/js/2.a63da593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dbef5ab69e88d31432",
+    "url": "/game-rock-scissors-paper/static/js/main.37096282.chunk.js"
+  },
+  {
+    "revision": "188e06a3102c88f51c17",
+    "url": "/game-rock-scissors-paper/static/js/runtime-main.acfcad30.js"
+  }
+]);
